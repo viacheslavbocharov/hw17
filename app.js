162a6d056656515c44cef2c5f5e122b516554476
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2024-02-29 11:26:30 +0000

    redo for focus

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 //Є текстове поле на сторінці. Під час фокусування на цьому полі збоку з'являється <div>. 
 //При пропажі фокусу - <div> так само пропадає
-let myElement = document.querySelector('.text');
+let myInput = document.querySelector('input[type="text"]');
 let myMessage = document.querySelector('.message');
 
 const toggleMessage = () => { myMessage.classList.toggle('active'); };
+//Hover effect
+// myInput.addEventListener("mouseover", toggleMessage)
+// myInput.addEventListener("mouseout", toggleMessage)
 
-myElement.addEventListener("mouseover", toggleMessage)
-myElement.addEventListener("mouseout", toggleMessage)
+
+//Focus
+myInput.addEventListener("focus", toggleMessage);
+myInput.addEventListener("blur", toggleMessage);
